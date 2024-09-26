527c90f3988641120714279773108b9a3e45d2eb
joacoleon <joacotleon@gmail.com>
2024-09-26 13:51:01 -0300

    Unifico front y back. Avances front.

---
+++ frontend/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>MEVN APP</v-app-bar-title>
+      <v-btn
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          slim
+          @click="onClick"
+        ></v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      temporary
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main >
+      <v-container>
+        <v-row>
+          <template v-for="n in 4" :key="n">
+            <v-col
+              class="mt-2"
+              cols="12"
+            >
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="6"
+              md="2"
+            >
+              <v-card height="150">asd</v-card>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+  const drawer = ref(false);
+  const theme = ref('light');
+
+  function onClick () {
+    theme.value = theme.value === 'light' ? 'dark' : 'light';
+  }
+</script>
+
+<script lang="ts">
+  export default {
+    data: () => ({ drawer: false }),
+  }
+</script>
+
+<style scoped>
+
+</style>
